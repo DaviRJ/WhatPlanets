ce42fafdfb961ccaafcd7ada5d4beac3dbe6a2f0
Davi Alves Bezerra <davico788@hotmail.com>
2019-07-06 10:33:50 -0300

    Refact: Using hooks on Window component

--- src/components/Window.js
+++ src/components/Window.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { insertPlanet } from "../reducers/actions";
 import axios from "axios";
@@ -7,21 +7,19 @@ import Config from "../configs/config";
 import Loading from "../assets/Loading";
 import Front from "./Front";
 
-class Window extends Component {
-    constructor(props) {
-        super(props);
+function Window({ insertPlanet, totalPlanets }) {
+    const [loading, setLoading] = useState(true);
 
-        this.state = {
-            loading: true
-        };
-    }
+    useEffect(() => {
+        fetchPlanets();
+    }, []);
 
-    fetchPlanets = async () => {
+    async function fetchPlanets() {
         let promissesArray = [];
         let urlsToCall = [];
 
-        //Mount de requests URLs
-        for (let i = 1; i <= Config.MAX_PLANETS_API; i++) {
+        //Mount the requests URLs
+        for (let i = 1; i <= totalPlanets; i++) {
             urlsToCall.push(
                 `${Config.BASE_API_URL + Config.PLANETS_API_URL}${i}`
             );
@@ -30,25 +28,22 @@ class Window extends Component {
         //Making the requests and storing in promisses array
         urlsToCall.map(url => promissesArray.push(axios.get(url)));
 
+        //awaint the requests and submiting the result to the store
         try {
             const result = await axios.all(promissesArray);
-            this.props.insertPlanet(result.map(({ data }) => data));
-            this.setState({ loading: false });
+            insertPlanet(result.map(({ data }) => data));
+            setLoading(false);
         } catch (error) {
             console.log("Oops, something went wrong: " + error.message);
         }
-    };
-
-    componentDidMount() {
-        this.fetchPlanets();
     }
 
-    render() {
-        return this.state.loading ? <Loading /> : <Front />;
-    }
+    return loading ? <Loading /> : <Front />;
 }
 
-const mapStateToProps = state => ({ planets: state.planetsReducer.planets });
+const mapStateToProps = state => ({
+    totalPlanets: state.planetsReducer.totalPlanets
+});
 
 const mapDispatchToProps = dispatch => ({
     insertPlanet: data => dispatch(insertPlanet(data))
